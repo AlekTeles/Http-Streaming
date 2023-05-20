4f081736deda90ec155e58b335da43c7d329e8c2
AlekTeles <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>
2023-05-20 19:29:28 -0300

    commit shesh

--- src/serve.js
+++ src/serve.js
@@ -20,12 +20,54 @@ import http  from 'node:http'
 // GET /users => Buscando usuários do back-end
 // POST /users => Criar um usuário no back-end
 
-const server = http.createServer((req, res) => {
+// Stateful - Stateless
+
+// JSON - Javascript Object Notation
+
+// Cabeçalhos (Requisição/resposta) => Metadados
+
+const users = []
+
+const server = http.createServer(async (req, res) => {
     const { method, url} = req
+
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers)
+        .toString())
+    } catch {
+        req.body = null
+    }
+
+    
+
+
+    if (method === 'GET'  && url === '/users') { 
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users)) 
+    }
+
+    if (method === 'POST' && url === '/users') {
+        const { name, email } = req.body
+
+        users.push({ 
+            id: 1,
+            name,
+            email,
+        }) 
+         
+        return res.writeHead(201).end() 
+    }
 
     console.log(method, url)
 
-    return res.end('Hello Wor')
+    return res.writeHead(404).end('Not Found')
 })
 
-server.listen(3333)
+server.listen(3333) 
